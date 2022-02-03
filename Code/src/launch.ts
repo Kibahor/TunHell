eb5690ded00d49cd18408673077a3f435dc7ac45
lublouin1 <lukas.blouin@etu.uca.fr>
2022-02-03 19:28:20 +0100

    Ajout de la fabrqiue de carte

--- Code/src/launch.ts
+++ Code/src/launch.ts
@@ -1,13 +1,12 @@
 import { Card } from './Card'
 import { CardWithBonus } from './CardWithBonus'
 import { CardType } from './CardType'
-import { SubType } from './SubType';
 
 
 //Je ne veux pas voir un seul "" car contrairement au '', il y a une interpretation par JS (=> donc execution plus longue)
 console.log('Hello World !');
-console.log(CardType.NAIN);
-let carte1 = new Card('Nain1',CardType.NAIN,SubType.GUERRIER,12,false,false);
+console.log(CardType.DWARF);
+let carte1 = new Card('Nain1',CardType.DWARF,12,false,false);
 carte1.printCard();
-let carte2 = new CardWithBonus('Piocheur1',CardType.NAIN,SubType.PIOCHEUR,2,5,true,false);
+let carte2 = new CardWithBonus('Piocheur1',CardType.DWARF,2,5,true,false);
 carte2.printCard();
