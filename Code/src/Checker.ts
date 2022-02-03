df2f7c980a44585cf7f0ac5b46a19335899cc0da
HandyS11 <62420910+HandyS11@users.noreply.github.com>
2022-02-03 19:14:07 +0100

    Ajout de toutes les classe basique du jeu, modification du diagramme de classe en conséquence. IL VA FALLOIR REVOIR BCP DE CHOSES !!

---
+++ Code/src/Checker.ts
@@ -0,0 +1,11 @@
+import { Card } from "./Card"
+import { Stack } from "./Stack"
+import { Player } from './Player/Player'
+
+export class Checker {
+
+    public checkCardToStack(p:Player, s:Stack, c:Card) : Boolean {
+        // Va y avoir du boulot ici, c'est la partie la plus chiante !!
+        return
+    }
+}
--- Code/src/GameManager.ts
+++ Code/src/GameManager.ts
@@ -1,9 +1,12 @@
 import { GameBoard } from './GameBoard'
+import { CardMover } from './CardMover';
 import { Player } from './Player/Player'
 
 export class GameManager {
+    public gameBoardList:Array<GameBoard> = [];
 
     public createGame() : GameBoard {
+        // Faut créer tout le système de génération de main automatique et aléatoire
         return 
     }
 
