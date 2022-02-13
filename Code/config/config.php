f7f3e44b69e113007d79b70759743c2d101ddf9b
Antoine <antoine.imbert@etu.uca.fr>
2022-02-13 19:43:35 +0100

    modif config array vue

--- Code/config/config.php
+++ Code/config/config.php
@@ -11,5 +11,6 @@ $mdp = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'; //voir lors la phase de pe
 $vues = array(
     'acceuil' => 'views/acceuil.php',
     'login' => 'views/login.php',
-    'sign' => 'views/signup.php'
-)
+    'sign' => 'views/signup.php',
+    'index' => 'index.php'
+);
