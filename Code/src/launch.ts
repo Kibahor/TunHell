40bc1051f50abeef87deaffce67ec4a5f3b6221f
HandyS11 <62420910+HandyS11@users.noreply.github.com>
2022-02-13 18:34:39 +0100

    Comment ce fais-ce ?

--- Code/src/launch.ts
+++
@@ -1,7 +0,0 @@
-import { Dwarf } from './Card/Dwarf'
-
-//Je ne veux pas voir un seul "" car contrairement au '', il y a une interpretation par JS (=> donc execution plus longue)
-console.log('Hello World !');
-let carte1 = new Dwarf('Dwarf',5,0,false,false);
-carte1.printCard();
-
