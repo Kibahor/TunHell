8e8d4b1d575c66f5f4b357e381ba719f53687cf9
Pedro Mezquita <pedramapedrama@gmail.com>
2022-02-13 18:37:34 +0100

    Gateway des comptes et des Parties

--- Code/config/config.php
+++ Code/config/config.php
@@ -3,8 +3,10 @@
 $rep = __DIR__.'/../'; //repertoire racine
 
 $base = 'mysql:host=localhost;dbname=DB_Tunhell_Accounts'; //BDD
-$user = 'DB_Tunhell';
-$mdp = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'; //voir lors la phase de pentest si ceci doit etre chiffré
+//$user = 'DB_Tunhell';
+//$mdp = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'; //voir lors la phase de pentest si ceci doit etre chiffré
+$user = 'root';
+$mdp = 'root';
 
 $vues['acceuil'] = 'views/acceuil.php';
 $vues['login'] = 'views/login.php';
---
+++ Code/controleur/AccountGateway.php
@@ -0,0 +1,31 @@
+<?php
+
+class AccountGateway extends Connection
+{
+    public function insert(Account $admin): int
+    {
+        $query = 'INSERT INTO adminn VALUES(:id, :pseudo, :avatar, :passwrd, :creationDate, :numberGames, :numberVictoires)';
+        $this->executeQuery($query, array(':id' => array($admin->getId(), PDO::PARAM_INT), ':pseudo' => array($admin->getPseudo(), PDO::PARAM_STR), ':avatar' => array($admin->getAvatar(), PDO::PARAM_STR), ':creationDate' => array($admin->getCreationDate(), PDO::PARAM_STR), ':numberGames' => array($admin->getNumberGames(), PDO::PARAM_INT), ':numberVictoires' => array($admin->getNumberVictoires(), PDO::PARAM_INT)));
+        return $this->lastInsertId();
+    }
+
+    public function delete(int $id): int
+    {
+        $query = 'DELETE FROM account WHERE id=:id';
+        $this->executeQuery($query, array(':id' => array($id, PDO::PARAM_INT)));
+        return $this->lastInsertId();
+    }
+
+    public function FindByPseudo(string $pseudo): Account
+    {
+        //preparation et execution de la requete sql (A APPRENDRE)
+        $query = 'SELECT * FROM adminn WHERE pseudo=:pseudo';
+        $this->executeQuery($query, array(':pseudo' => array($pseudo, PDO::PARAM_STR)));
+
+        //conversion en objets
+        $resultats = $this->getResults();
+
+        $admin = new Admin($resultats[0]['id'], $resultats[0]['pseudo'], $resultats[0]['avatar'], $resultats[0]['password'],  $resultats[0]['creationDate'],  $resultats[0]['numberGames'], $resultats[0]['numberVictoires']);
+        return $admin;
+    }
+}
