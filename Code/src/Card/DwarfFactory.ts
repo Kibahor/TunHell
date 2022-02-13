8a7b9cb010f42a82cb23cf8ef9d9c0f2f9967a12
lublouin1 <lukas.blouin@etu.uca.fr>
2022-02-13 19:59:24 +0100

    Correction des vérification du json

--- Code/src/Card/DwarfFactory.ts
+++ Code/src/Card/DwarfFactory.ts
@@ -7,7 +7,7 @@ export class DwarfFactory extends CardFactory {
     public CreateCard(receipe:JSON, nb:number) : Array<Card> {
 
         if (!this.verify_receipe(receipe)) {
-            throw new TypeError("La recette donnée n'est pas compatible !");
+            throw `Problème création de la carte Dwarf (nb:${nb})`;
         }
         let cards : Array<Card> = [];
         for (let i = 0; i < nb; i++) {
@@ -16,39 +16,36 @@ export class DwarfFactory extends CardFactory {
         return cards;
     }
 
-    public verify_receipe(receipe:JSON) : boolean {
+    public verify_receipe(receipe:JSON) : boolean { //Vrai ok et Faux ko
         try {
-            if (!(
-                receipe['first_value'] === Number &&
-                receipe['second_value'] === Number &&
-                receipe['up_symbol'] === Boolean &&
-                receipe['stop_symbol'] === Boolean
-            )) {
-                throw "Invalid type!";
-            }
-            // Règle :
-            // Si les 2 valeurs sont supérieures à 0 alors les symboles sont forcéments faux 
-            if (!(
-                (receipe['first_value'] > 0 && receipe['second_value'] > 0)
-                && !receipe['up_symbol'] && !receipe['stop_symbol']
-            )) {
-                throw "Invalid logic!";
-            }
-            // Règle :
-            // 2 symboles ne peuvent être vrais à la fois
-            if (!(
-                (receipe['up_symbol'] && !receipe['stop_symbol'])
-                ||
-                (!receipe['up_symbol'] && receipe['stop_symbol'])
-                ||
-                (receipe['up_symbol'] && receipe['stop_symbol'])
-            )) {
-                throw "Invalid logic! (2)";
-            }
-        }
-        catch (err) {
+            this.assertType(receipe);
+            this.assertSymbole(receipe);
+            this.assertValueAndSymbole(receipe);
+        } catch (err) {
+            console.error(err);
             return false;
         }
         return true;
+    }
+
+    private assertType(receipe:JSON):void { // Vérification des clés et des types
+          if (!(
+            typeof receipe['first_value'] === 'number' &&
+            typeof receipe['second_value'] === 'number' &&
+            typeof receipe['up_symbol'] === 'boolean' &&
+            typeof receipe['stop_symbol'] === 'boolean'
+          )) throw 'Invalid type : une ou plusieurs valeurs du json ne contiennent pas le bon type ou il manque une clé';
+    }
+
+    assertSymbole(receipe:JSON):void {
+        if ((receipe['up_symbol'] && receipe['stop_symbol']) === true)
+            throw "Invalid logic : une carte peut pas avoir les deux symboles en même temps"
+    }
+
+    assertValueAndSymbole(receipe:JSON):void {
+        if (
+            (receipe['first_value'] > 0 && receipe['second_value'] > 0) &&
+            (receipe['up_symbol'] || receipe['stop_symbol'])
+        ) throw 'Invalid logic : si une carte a deux valeurs, il ne peut pas avoir de symboles !';
     }
 }
