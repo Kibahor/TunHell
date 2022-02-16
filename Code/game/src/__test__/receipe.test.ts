fecfabcfe727bd60b3b5d79c6ba39d9baef6c032
lublouin1 <lukas.blouin@etu.uca.fr>
2022-02-16 13:26:33 +0100

    Modification des test unitaires

--- Code/game/src/__test__/receipe.test.ts
+++ Code/game/src/__test__/receipe.test.ts
@@ -17,12 +17,13 @@ function loadReceipes(map:Map<string,JSON>,folder:string){
 
 function sortReceipes(receipes:Map<string,JSON>,deckName:string):Map<string,JSON>{
     let result = new Map();
-    for(let [factoryType,type] of Object.entries(receipes.get('Deck')[deckName])){ // "Dwarf" et {}
+    for(let [factoryType,type] of Object.entries(receipes.get('Deck')[deckName])){ // "Warrior" et {}
       for(let [name,number] of Object.entries(type)){
+        let toAdd = receipes.get(name) //Ajout le type (Warrior, Picker...)
         if (!result.has(factoryType))
-            result.set(factoryType,[receipes.get(name)])
+            result.set(factoryType,[toAdd])
         else
-            result.set(factoryType,result.get(factoryType).concat(receipes.get(name)))
+            result.get(factoryType).push(toAdd)
       }
     }
     return result;
@@ -31,45 +32,30 @@ function sortReceipes(receipes:Map<string,JSON>,deckName:string):Map<string,JSON
 let path_json:string = path.resolve(path.join('./res','/Receipe/'));
 let receipes:Map<string,JSON> = new Map();
 receipes = loadReceipes(receipes,path_json);
+console.log(receipes)
 receipes = sortReceipes(receipes,'Default');
-
+console.log(receipes)
 //Dwarf
 let receipesDwarf = receipes.get('Dwarf')
+console.log(receipesDwarf)
 
-for(let [type,name] of Object.entries(receipesDwarf)){
-  for(let [value,receipe]of Object.entries(name)){
-    describe(`check type of value for ${value}`, () => {
-      it("first_value should be number", () => {
-        expect(typeof receipe['first_value']).toBe('number');
-      });
-
-      it("second_value should be number", () => {
-        expect(typeof receipe['second_value']).toBe('number');
-      });
-
-      it("up_symbol should be boolean", () => {
-        expect(typeof receipe['up_symbol']).toBe('boolean');
-      });
-
-      it("stop_symbol should be boolean", () => {
-        expect(typeof receipe['stop_symbol']).toBe('boolean');
-      });
+describe(`check type of value for Dwarf cards`, () => {
+  for(let [type,name] of Object.entries(receipesDwarf)){
+    describe(`Check type of value for ${type}`, () => {
+      for(let [value,receipe]of Object.entries(name)){
+        it(`Type are correct for ${value}`, () =>{
+          expect(typeof receipe['first_value']).toBe('number'); //first_value should be number
+          expect(typeof receipe['second_value']).toBe('number'); //second_value should be number
+          expect(typeof receipe['up_symbol']).toBe('boolean'); //up_symbol should be boolean
+          expect(typeof receipe['stop_symbol']).toBe('boolean'); //stop_symbol should be boolean
+        });
+      }
     });
   }
-}
+});
 
 
 /*
-//Test Unitaire (convertir pour utiiser avec jest)
-function assertType(receipe:JSON):void { // Vérification des clés et des types
-    if (!(
-      typeof receipe['first_value'] === 'number' &&
-      typeof receipe['second_value'] === 'number' &&
-      typeof receipe['up_symbol'] === 'boolean' &&
-      typeof receipe['stop_symbol'] === 'boolean'
-    )) throw 'Invalid type : une ou plusieurs valeurs du json ne contiennent pas le bon type ou il manque une clé';
-}
-
 function assertSymbole(receipe:JSON):void {
   if ((receipe['up_symbol'] && receipe['stop_symbol']) === true)
       throw "Invalid logic : une carte peut pas avoir les deux symboles en même temps"
