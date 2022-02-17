07e21c227ccd4fb46d5157d0dba9ffbe77239b05
lublouin1 <lukas.blouin@etu.uca.fr>
2022-02-17 13:15:56 +0100

    Test unitaire des carte Dwarf fini !

--- Code/game/src/__test__/receipe.test.ts
+++ Code/game/src/__test__/receipe.test.ts
@@ -6,8 +6,8 @@ function loadReceipes(map:Map<string,JSON>,folder:string){
         let path_file = path.join(folder,file);
         if(fs.lstatSync(path_file).isDirectory()){
             map=loadReceipes(map,path_file);
-        }else if(path.posix.extname(path_file) == '.json'){
-            let key = path.posix.basename(path_file,'.json');
+        }else if(path.extname(path_file) == '.json'){
+            let key = path.basename(path_file,'.json');
             let value = JSON.parse(fs.readFileSync(path_file));
             map.set(key, value);
         }
@@ -15,58 +15,60 @@ function loadReceipes(map:Map<string,JSON>,folder:string){
     return map
 }
 
-function sortReceipes(receipes:Map<string,JSON>,deckName:string):Map<string,JSON>{
+function sortReceipes(receipes:Map<string,JSON>,deckName:string):Map<string,Map<string,JSON>>{
     let result = new Map();
     for(let [factoryType,type] of Object.entries(receipes.get('Deck')[deckName])){ // "Warrior" et {}
       for(let [name,number] of Object.entries(type)){
-        let toAdd = receipes.get(name) //Ajout le type (Warrior, Picker...)
         if (!result.has(factoryType))
-            result.set(factoryType,[toAdd])
+            result.set(factoryType,new Map([[name,receipes.get(name)]]))
         else
-            result.get(factoryType).push(toAdd)
+            result.get(factoryType).set(name,receipes.get(name))
       }
     }
     return result;
 }
 
 let path_json:string = path.resolve(path.join('./res','/Receipe/'));
-let receipes:Map<string,JSON> = new Map();
-receipes = loadReceipes(receipes,path_json);
-console.log(receipes)
-receipes = sortReceipes(receipes,'Default');
-console.log(receipes)
-//Dwarf
-let receipesDwarf = receipes.get('Dwarf')
-console.log(receipesDwarf)
+let receipes:Map<string,JSON> = loadReceipes(new Map(),path_json); //Récursive d'où le new Map()
+let sortedReceipes:Map<string,Map<string,JSON>> = sortReceipes(receipes,'Default');
 
-describe(`check type of value for Dwarf cards`, () => {
-  for(let [type,name] of Object.entries(receipesDwarf)){
-    describe(`Check type of value for ${type}`, () => {
-      for(let [value,receipe]of Object.entries(name)){
-        it(`Type are correct for ${value}`, () =>{
-          expect(typeof receipe['first_value']).toBe('number'); //first_value should be number
-          expect(typeof receipe['second_value']).toBe('number'); //second_value should be number
-          expect(typeof receipe['up_symbol']).toBe('boolean'); //up_symbol should be boolean
-          expect(typeof receipe['stop_symbol']).toBe('boolean'); //stop_symbol should be boolean
+//Dwarf
+let receipesDwarf:Map<string,JSON> = sortedReceipes.get('Dwarf')
+describe(`Check Dwarf cards`, () => {
+  for(let [type,map] of receipesDwarf.entries()){
+    describe(`Check for ${type}`, () => {
+      for(let [name,receipe]of Object.entries(map)){
+        describe(`Check for ${name}`, () => {
+          checkType(name,receipe);
+          checkLogic(name,receipe);
         });
       }
     });
   }
 });
 
-
-/*
-function assertSymbole(receipe:JSON):void {
-  if ((receipe['up_symbol'] && receipe['stop_symbol']) === true)
-      throw "Invalid logic : une carte peut pas avoir les deux symboles en même temps"
+function checkType(name:string,receipe:JSON):void{
+    it(`Types of value`, () =>{
+      expect(typeof receipe['first_value']).toBe('number'); //first_value should be number
+      expect(typeof receipe['second_value']).toBe('number'); //second_value should be number
+      expect(typeof receipe['up_symbol']).toBe('boolean'); //up_symbol should be boolean
+      expect(typeof receipe['stop_symbol']).toBe('boolean'); //stop_symbol should be boolean
+    });
 }
 
-function assertValueAndSymbole(receipe:JSON):void {
-  if (
-      (receipe['first_value'] > 0 && receipe['second_value'] > 0) &&
-      (receipe['up_symbol'] || receipe['stop_symbol'])
-  ) throw 'Invalid logic : si une carte a deux valeurs, il ne peut pas avoir de symboles !';
-}*/
+function checkLogic(name:string,receipe:JSON):void{
+    //une carte peut pas avoir les deux symboles en même temps
+    it(`Logic of symbole`, () =>{
+      expect(receipe['up_symbol'] && receipe['stop_symbol']).toBeFalsy
+    });
 
+    //si une carte a deux valeurs, il ne peut pas avoir de symboles
+    it(`Logic of value and symbole`, () =>{
+      expect(
+        (receipe['first_value'] > 0 && receipe['second_value'] > 0) &&
+        (receipe['up_symbol'] || receipe['stop_symbol'])
+      ).toBeFalsy
+    });
+}
 
 
