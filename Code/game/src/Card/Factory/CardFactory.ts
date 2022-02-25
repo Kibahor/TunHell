c8fa011be1d1155fd230986c93216f202f9a9c50
lublouin1 <lukas.blouin@etu.uca.fr>
2022-02-25 16:34:45 +0100

    Supression de verify_card (test unitaire)

--- Code/game/src/Card/Factory/CardFactory.ts
+++ Code/game/src/Card/Factory/CardFactory.ts
@@ -1,5 +1,4 @@
 import { Card } from "../Card";
 export abstract class CardFactory {
-    protected abstract verify_receipe(receipe:JSON) : boolean;
     abstract CreateCard(typename:string, receipe:JSON, nb:number) : Array<Card>;
 }
--- Code/game/src/Card/Factory/DwarfFactory.ts
+++ Code/game/src/Card/Factory/DwarfFactory.ts
@@ -5,48 +5,10 @@ import { Dwarf } from "../Dwarf";
 export class DwarfFactory extends CardFactory {
 
     public CreateCard(typeName:string, receipe:JSON, nb:number) : Array<Card> {
-
-        if (!this.verify_receipe(receipe)) {
-            throw `Problème création de la carte Dwarf (nb:${nb})`;
-        }
         let cards : Array<Card> = [];
         for (let i = 0; i < nb; i++) {
             cards.push(new Dwarf(typeName, receipe['first_value'], receipe['second_value'], receipe['up_symbol'], receipe['stop_symbol']));
         }
         return cards;
-    }
-
-    public verify_receipe(receipe:JSON) : boolean { //Vrai ok et Faux ko
-        try {
-            this.assertType(receipe);
-            this.assertSymbole(receipe);
-            this.assertValueAndSymbole(receipe);
-        } catch (err) {
-            console.error(err);
-            return false;
-        }
-        return true;
-    }
-
-    //Test Unitaire (convertir pour utiiser avec jest)
-    private assertType(receipe:JSON):void { // Vérification des clés et des types
-          if (!(
-            typeof receipe['first_value'] === 'number' &&
-            typeof receipe['second_value'] === 'number' &&
-            typeof receipe['up_symbol'] === 'boolean' &&
-            typeof receipe['stop_symbol'] === 'boolean'
-          )) throw 'Invalid type : une ou plusieurs valeurs du json ne contiennent pas le bon type ou il manque une clé';
-    }
-
-    private assertSymbole(receipe:JSON):void {
-        if ((receipe['up_symbol'] && receipe['stop_symbol']) === true)
-            throw "Invalid logic : une carte peut pas avoir les deux symboles en même temps"
-    }
-
-    private assertValueAndSymbole(receipe:JSON):void {
-        if (
-            (receipe['first_value'] > 0 && receipe['second_value'] > 0) &&
-            (receipe['up_symbol'] || receipe['stop_symbol'])
-        ) throw 'Invalid logic : si une carte a deux valeurs, il ne peut pas avoir de symboles !';
     }
 }
