1814ef0ffbf709e39b224c25c34169275be717e0
lublouin1 <lukas.blouin@etu.uca.fr>
2022-03-07 11:10:07 +0100

    Les carte sont maintenant créé par tas et non par type

--- Code/game/src/Card/Factory/ReceipeFunctions.ts
+++ Code/game/src/Card/Factory/ReceipeFunctions.ts
@@ -2,34 +2,29 @@ const fs = require('fs');
 const path = require('path');
 const PATH_JSONS:string = path.resolve(__dirname,'../../../res/Receipe/');
 
-function loadReceipes(map:Map<string,JSON>,folder:string){
+function loadReceipes(map:Map<string,Map<string,JSON>>,folder:string){
     for(let file of fs.readdirSync(folder)){
         let path_file = path.join(folder,file);
         if(fs.lstatSync(path_file).isDirectory()){
-            map=loadReceipes(map,path_file);
+            map = loadReceipes(map,path_file);
         }else if(path.extname(path_file) == '.json'){
-            let key = path.basename(path_file,'.json');
+            let directory = path.basename(path.dirname(path_file))
+            let filename = path.basename(path_file,'.json');
             let value = JSON.parse(fs.readFileSync(path_file));
-            map.set(key, value);
+            if(!map.has(directory))
+                map.set(directory, new Map().set(filename,value));
+            else
+                map.get(directory).set(filename,value)
         }
     }
     return map
 }
 
-export function sortReceipes(receipes:Map<string,JSON>,deck:JSON):Map<string,Map<string,JSON>>{
-    let result = new Map();
-    for(let [factoryType,type] of Object.entries(deck)){ // "Warrior" et {}
-      for(let [name,number] of Object.entries(type)){
-        if (!result.has(factoryType))
-            result.set(factoryType,new Map([[name,receipes.get(name)]]))
-        else
-            result.get(factoryType).set(name,receipes.get(name))
-      }
-    }
-    return result;
+export function json2map(json):Map<any,any>{
+    return new Map(Object.entries(json))
 }
 
-export let receipes:Map<string,JSON> = loadReceipes(new Map(),PATH_JSONS); //Récursive d'où le new Map()
+export let receipes:Map<string,Map<string,JSON>> = loadReceipes(new Map(),PATH_JSONS); //Récursive d'où le new Map()
 export function getDeck(deckName:string):JSON {
-    return receipes.get('Deck')[deckName]; 
+    return json2map(receipes.get('Receipe').get('Deck')).get(deckName); 
 }
